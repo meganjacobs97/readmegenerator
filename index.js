3eb4022cbdd7825ec4ff49b33755963aafbfe4a5
Megan Jacobs <meganjacobs@live.com>
2020-04-01 09:56:52 -0700

    made inital prompt and api call

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 //IMPORT LIBRARIES 
-// for hiding the keys 
-require('dotenv').config; 
 //for api call
 const axios = require("axios");
 //for prompting 
@@ -9,86 +7,40 @@ const inquirer = require("inquirer");
 const util = require("util"); 
 const fs = require("fs"); 
 
+
+
 //TODO
 inquirer
-.prompt([
-    {
-      type: "input",
-      message: "What is your name?",
-      name: "name"
-    },
-    {
-      type: "checkbox",
-      message: "What languages do you know?",
-      name: "languages",
-      choices: ["HTML","JavaScript","CSS"]
-    },
-    {
-      type: "list",
-      message: "What is your preferred method of communciation?",
-      name: "communication",
-      choices: ["email","text","phone"]
-    }
-  ])
+.prompt({
+  message: "Enter your GitHub username",
+  name: "username"
+})
+.then(function({ username }) {
+  const queryUrl = `https://api.github.com/users/${username}`;
+  axios
+  .get(queryUrl)
   .then(function(response) {
-    
-    var fileName = response.name + ".json"; 
-    var data = JSON.stringify(response,null,"\t"); //params: data, replacer, formatting  
-
-    fs.writeFile(fileName,data,function(error) {
-        if(error) {
-            return console.log(error); 
-        }
-    }); 
+    const email = response.data.email; 
+    const pfpURL = response.data.avatar_url; 
+    console.log(response); 
+    console.log(email); 
+    console.log(pfpURL); 
   });
-
-  const config = { headers: { accept: "application/json" } };
-
-//TODO
-axios
-  .get("https://icanhazdadjoke.com/", config)
-  .then(function(res) {
-    const { joke } = res.data;
-
-    return appendFileAsync("jokes.txt", joke + "\n");
-  })
-  .then(function() {
-    return readFileAsync("jokes.txt", "utf8");
-  })
-  .then(function(data) {
-    console.log("Saved dad jokes:");
-    console.log(data);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
-
-  //TODO
-  inquirer
-  .prompt({
-    message: "Enter your GitHub username",
-    name: "username"
-  })
-  .then(function({ username }) {
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-    axios
-    .get(queryUrl)
-    .then(function(res) {
-      const names = []; 
-      
-      for(let i = 0; i < res.data.length; i++) {
-        names.push(res.data[i].name); 
-      }
-
-      const namesStr = names.join("\n"); 
-
-      fs.writeFile("repos.txt",namesStr,function(error) {
-        if(error) {
-          return console.log("error"); 
-        }
-        console.log(names.length + " repos"); 
-      });
-    });
-  });
+});
 
 
+// The user will be prompted for their GitHub username, which will be used to make a call to the GitHub API to retrieve their email and profile image. They will then be prompted with questions about their project.
+// The README will be populated with the following:
+
+// * At least one badge
+// * Project title
+// * Description
+// * Table of Contents
+// * Installation
+// * Usage
+// * License
+// * Contributing
+// * Tests
+// * Questions
+//   * User GitHub profile picture
+//   * User GitHub email
