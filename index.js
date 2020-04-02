e4f8cf3f0d9e35a9aa6c0868f0294f8ae2e784be
Megan Jacobs <meganjacobs@live.com>
2020-04-01 19:43:53 -0700

    sorted code into functions

--- index.js
+++ index.js
@@ -7,24 +7,8 @@ const inquirer = require("inquirer");
 const util = require("util"); 
 const fs = require("fs"); 
 
-
-// * The generated README includes a bio image from the user's GitHub profile.
-
-// * The generated README includes the user's email.
-
-// * The generated README includes the following sections: 
-//   * Title
-//   * Description
-//   * Table of Contents
-//   * Installation
-//   * Usage
-//   * License
-//   * Contributing
-//   * Tests
-//   * Questions
-//TODO
-inquirer
-.prompt([
+//array for user input choices 
+const inputArr = [
   {
     type: "input",
     message: "Enter your GitHub username",
@@ -76,7 +60,81 @@ inquirer
     message: "Enter any questions for your application",
     name: "questions"
   }
-])
+]
+
+//function that builds and returns the table of contents for the readme
+function getTableOfContents(response) {
+  let contents = ""; 
+
+  if(response.description !== "") {
+    contents += "\n* [Description](#description)"; 
+  }
+  if(response.installation !== "") {
+    contents += "\n* [Installation](#installation)"; 
+  }
+  if(response.usage !== "") {
+    contents += "\n* [Usage](#usage)"; 
+  }
+  if(response.credits !== "") {
+    contents += "\n* [Credits](#credits)"; 
+  }
+  if(response.license !== "") {
+    contents += "\n* [License](#license)"; 
+  }
+  if(response.contributing !== "") {
+    contents += "\n* [Contributing](#contributing)"; 
+  }
+  if(response.tests !== "") {
+    contents += "\n* [Tests](#tests)"; 
+  }
+  if(response.questions !== "") {
+    contents += "\n* [Questions](#questions)"; 
+  }
+  return contents; 
+}
+
+//function that builds and returns the string for the file 
+function getFileData(response,contents,pfpURL,email) {
+  let fileData = ``; 
+  if(response.title !== "") {
+    fileData += `# ${response.title}\n\n`
+  }
+  if(response.description !== "") {
+    fileData += `${response.description}\n\n`
+  }
+  fileData += `## Table of Contents\n\n${contents}\n\n`
+  if(response.installation !== "") {
+    fileData += `## Installation\n\n${response.installation}\n\n`
+  }
+  if(response.usage !== "") {
+    fileData += `## Usage\n\n${response.usage}\n\n`
+  }
+  if(response.credits !== "") {
+    fileData += `## Credits\n\n${response.credits}\n\n`
+  }
+  if(response.license !== "") {
+    let license = response.license; 
+    if(license.includes(" ")) {
+      license = license.replace(" ","+");
+    }
+    fileData += `## License\n\n![license](https://img.shields.io/static/v1?label=license&message=${license}&color=green)\n\n`
+  }
+  if(response.contributing !== "") {
+    fileData += `## Contributing\n\n${response.contributing}\n\n`
+  }
+  if(response.tests !== "") {
+    fileData += `## Tests\n\n${response.tests}\n\n`
+  }
+  if(response.questions !== "") {
+    fileData += `## Questions\n\n${response.questions}\n\n\n\n`
+  }
+  return fileData; 
+}
+
+
+//prompt user and call github api
+inquirer
+.prompt(inputArr)
 .then(function(response) {
   const queryUrl = `https://api.github.com/users/${response.username}`;
   axios
@@ -85,70 +143,12 @@ inquirer
     const email = githubResponse.data.email; 
     const pfpURL = githubResponse.data.avatar_url; 
     
-
     //build table of contents based on answers we have recieved 
-    let contents = ""; 
-
-    if(response.description !== "") {
-      contents += "\n* [Description](#description)"; 
-    }
-    if(response.installation !== "") {
-      contents += "\n* [Installation](#installation)"; 
-    }
-    if(response.usage !== "") {
-      contents += "\n* [Usage](#usage)"; 
-    }
-    if(response.credits !== "") {
-      contents += "\n* [Credits](#credits)"; 
-    }
-    if(response.license !== "") {
-      contents += "\n* [License](#license)"; 
-    }
-    if(response.contributing !== "") {
-      contents += "\n* [Contributing](#contributing)"; 
-    }
-    if(response.tests !== "") {
-      contents += "\n* [Tests](#tests)"; 
-    }
-    if(response.questions !== "") {
-      contents += "\n* [Questions](#questions)"; 
-    }
-
+    let contents = getTableOfContents(response); 
 
     //build string for site 
-    let fileData = ``; 
-    if(response.title !== "") {
-      fileData += `# ${response.title}\n\n`
-    }
-    if(response.description !== "") {
-      fileData += `${response.description}\n\n`
-    }
-    fileData += `## Table of Contents\n\n${contents}\n\n`
-    if(response.installation !== "") {
-      fileData += `## Installation\n\n${response.installation}\n\n`
-    }
-    if(response.usage !== "") {
-      fileData += `## Usage\n\n${response.usage}\n\n`
-    }
-    if(response.credits !== "") {
-      fileData += `## Credits\n\n${response.credits}\n\n`
-    }
-    if(response.license !== "") {
-      let license = response.license; 
-      if(license.includes(" ")) {
-        license = license.replace(" ","+");
-      }
-      fileData += `## License\n\n![license](https://img.shields.io/static/v1?label=license&message=${license}&color=green)\n\n`
-    }
-    if(response.contributing !== "") {
-      fileData += `## Contributing\n\n${response.contributing}\n\n`
-    }
-    if(response.tests !== "") {
-      fileData += `## Tests\n\n${response.tests}\n\n`
-    }
-    if(response.questions !== "") {
-      fileData += `## Questions\n\n${response.questions}\n\n\n\n`
-    }
+    let fileData = getFileData(response,contents); 
+
     //add in picture and email 
     if(pfpURL) {
       fileData += `![GitHub Profile Picture](${pfpURL}%=50x50)\n\n`
@@ -168,20 +168,3 @@ inquirer
     }); 
   });
 });
-
-
-// The user will be prompted for their GitHub username, which will be used to make a call to the GitHub API to retrieve their email and profile image. They will then be prompted with questions about their project.
-// The README will be populated with the following:
-
-// * At least one badge
-// * Project title
-// * Description
-// * Table of Contents
-// * Installation
-// * Usage
-// * License
-// * Contributing
-// * Tests
-// * Questions
-//   * User GitHub profile picture
-//   * User GitHub email
